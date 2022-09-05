124a4f2f5a6f898298083da359443d3e0cf750d7
Sebastian_B <basti.boehler@hotmail.de>
2022-09-05 09:17:24 +0200

    Update index.ts

--- src/mysql/index.ts
+++ src/mysql/index.ts
@@ -14,16 +14,9 @@ class sql_class {
     pool: mysql.Pool;
 
     constructor(database: string) {
-        console.log({
-            host: process.env.SQL_HOST,
-            user: process.env.SQL_USER,
-            password: process.env.SQL_PASSWORD,
-            port: +(process.env.SQL_PORT || 3306),
-            database: database,
-            path
-        })
-
         this.pool = mysql.createPool({
+            connectionLimit: 35,
+            queueLimit: 100,
             host: process.env.SQL_HOST,
             user: process.env.SQL_USER,
             password: process.env.SQL_PASSWORD,
