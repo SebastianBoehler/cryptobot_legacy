928479198f5f53598db67b581d4be9cab976b654
Sebastian_B <basti.boehler@hotmail.de>
2022-09-05 09:04:43 +0200

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,33 +2,24 @@ module.exports = {
     apps: [{
         name: "ftx_database",
         script: "./build/ftx/database.js",
-        env_prod: {
+        env: {
             NODE_ENV: "prod"
-        },
-        env_dev: {
-            NODE_ENV: "dev"
         },
         watch: true
     },
     {
         name: "ftx_backtester",
         script: "./build/ftx/backtester.js",
-        env_prod: {
+        env: {
             NODE_ENV: "prod"
-        },
-        env_dev: {
-            NODE_ENV: "dev"
         },
         watch: true
     },
     {
         name: "server",
         script: "./build/server.js",
-        env_prod: {
+        env: {
             NODE_ENV: "prod"
-        },
-        env_dev: {
-            NODE_ENV: "dev"
         },
         watch: true
     }
