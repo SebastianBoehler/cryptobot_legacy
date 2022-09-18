6f19320c58f11c6790f8ee52cc23fada7b3656de
Sebastian_B <basti.boehler@hotmail.de>
2022-09-18 19:55:59 +0200

    Update backtester.ts

--- src/ftx/backtester.ts
+++ src/ftx/backtester.ts
@@ -11,7 +11,7 @@ const sqlClientStorage = new mysql('storage');
 const startTime = new Date();
 startTime.setDate(startTime.getDate() - 30);
 //startTime.setHours(startTime.getHours() - 15);
-const rulesToTest = ['test', 'test2', 'test3', 'test4', 'test5', 'test6']
+const rulesToTest = ['test', 'test2', 'test3', 'test4', 'test5', 'test6', 'test7']
 let startInvest = 500
 const leverage = +(process.env.LEVERAGE || 5);
 let endTime
