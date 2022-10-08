e7ffd34eff366950c2b4f6709088d284c7408b8b
Sebastian_B <basti.boehler@hotmail.de>
2022-10-08 22:49:54 +0200

    Update backtester.ts

--- src/ftx/backtester.ts
+++ src/ftx/backtester.ts
@@ -16,7 +16,7 @@ const sqlClientStorage = new mysql('storage');
 const startTime = new Date();
 startTime.setDate(startTime.getDate() - 35);
 //startTime.setHours(startTime.getHours() - 0.2);
-const rulesToTest = ['test', 'test2', 'test3', 'test4', 'test5', 'test6', 'test7', 'test8', 'test9', 'test10', 'test11', 'test12', 'test13', 'test14', 'test15', 'test16', 'test17', 'test18']
+const rulesToTest = ['test', 'test2', 'test3', 'test4', 'test5', 'test6', 'test7', 'test8', 'test9', 'test10', 'test11', 'test12', 'test13', 'test14', 'test15', 'test16', 'test17', 'test18', 'test19']
 let startInvest = 500
 const leverage = +(process.env.LEVERAGE || 5);
 let endTime: number | undefined
@@ -446,6 +446,32 @@ async function main() {
                             price <= indicators25min['bollingerBands']['lower']
                        ]]
                     },
+                    'test19': {
+                        'Long Entry': [[
+                            !waitAfterLoss,
+                            price < indicators25min['bollingerBands']['lower'],
+                        ], [
+                            price > indicators25min['bollingerBands']['lower'],
+                            indicators60min['MACD']['histogram']! > indicators60min['MACD_prev']['histogram']!,
+                            indicators25min['MACD']['histogram']! > 0
+                        ]],
+                       'Long Exit': [[
+                            profitThreshold3 ||
+                            price >= indicators25min['bollingerBands']['upper']
+                       ]],
+                       'Short Entry': [[
+                            !waitAfterLoss,
+                            price > indicators25min['bollingerBands']['upper'],
+                       ], [
+                            price < indicators25min['bollingerBands']['upper'],
+                            indicators60min['MACD']['histogram']! < indicators60min['MACD_prev']['histogram']!,
+                            indicators25min['MACD']['histogram']! < 0
+                       ]],
+                       'Short Exit': [[
+                            profitThreshold3 ||
+                            price <= indicators25min['bollingerBands']['lower']
+                       ]]
+                    },
                 }
 
                 if (!rules[rule]) continue
@@ -469,6 +495,8 @@ async function main() {
                     '60m ADX pdi / mdi': indicators60min['ADX']['pdi'] / indicators60min['ADX']['mdi'],
                     '60m ADL': indicators60min['ADL'],
                     '60m volume': indicators60min['volume'],
+                    '60m close / open': indicators60min['close'] / indicators60min['open'],
+                    '60m high / low': indicators60min['high'] / indicators60min['low'],
                 }
 
                 //there is an entry
@@ -653,7 +681,7 @@ async function main() {
     
     setTimeout(() => {
         main()
-    }, 1000 * 60 * 60);
+    }, 1000 * 60 * 60 * 4);
 }
 
 main()
