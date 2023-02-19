91c0e64cbddfb0f8ad84bcd2db09cbcbe09bc331
Sebastian_B <basti.boehler@hotmail.de>
2023-02-19 15:23:42 +0100

    fix: server over http again

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,5 @@
 import express, { Express } from 'express';
 import cors from 'cors';
-import fs from 'fs';
 import * as dotenv from 'dotenv';
 dotenv.config({
     path: `${process.env.NODE_ENV?.split(' ').join('')}.env`
@@ -12,15 +11,14 @@ const port = process.env.PORT || 3001;
 import ftxRoutes from './ftx/routes';
 import mongoRoutes from './mongodb/routes';
 import { logger } from './utils';
-import https from 'https';
 
 server.use(cors())
 server.use(express.json());
 
 const middleware = (req: Request, res: any, next: any) => {
     logger.http(`Received ${req.method} request for ${req.url}`);
-    //const cacheInSeconds = 30
-    //res.set('Cache-control', `public, max-age=${cacheInSeconds}`)
+    const cacheInSeconds = 30
+    res.set('Cache-control', `public, max-age=${cacheInSeconds}`)
     next();
 };
 
@@ -29,12 +27,6 @@ server.use(middleware);
 server.use('/ftx', ftxRoutes);
 server.use('/mongodb', mongoRoutes);
 
-https.createServer(
-    {
-        key: fs.readFileSync('key.pem'),
-        cert: fs.readFileSync('cert.pem')
-    },
-    server
-).listen(port, () => {
-    logger.info(`Server listening on port ${port}`);
+server.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 });
