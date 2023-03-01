ceff3a9d3a5a5850dcf9bebc961bbed8726e31fa
Sebastian Boehler <basti.boehler@hotmail.de>
2023-03-01 21:13:36 +0100

    fix: backtester in pm2 config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,53 +1,64 @@
 module.exports = {
-    apps: [{
-        name: "dydx_database",
-        script: "./build/dydx/database.js",
-        env_prod: {
-            NODE_ENV: "prod"
-        },
-        env_dev: {
-            NODE_ENV: "dev"
-        },
-        watch: false
+  apps: [{
+    name: "dydx_database",
+    script: "./build/dydx/database.js",
+    env_prod: {
+      NODE_ENV: "prod"
     },
-    {
-        name: "coinbase_database",
-        script: "./build/coinbase/database.js",
-        env_prod: {
-            NODE_ENV: "prod",
-        },
-        env_dev: {
-            NODE_ENV: "dev",
-        },
-        //exec_mode: "cluster",
-        //instances: "max",
-        watch: false
+    env_dev: {
+      NODE_ENV: "dev"
     },
-    {
-        name: "binance_database",
-        script: "./build/binance/database.js",
-        env_prod: {
-            NODE_ENV: "prod",
-        },
-        env_dev: {
-            NODE_ENV: "dev",
-        },
-        //exec_mode: "cluster",
-        //instances: "max",
-        watch: false
+    watch: false
+  },
+  {
+    name: "coinbase_database",
+    script: "./build/coinbase/database.js",
+    env_prod: {
+      NODE_ENV: "prod",
     },
-    {
-        name: "server",
-        script: "./build/server.js",
-        env_prod: {
-            NODE_ENV: "prod",
-            PORT: 80
-        },
-        env_dev: {
-            NODE_ENV: "dev"
-        },
-        watch: false,
-        exec_mode: "cluster",
-        instances: "max",
-    }],
+    env_dev: {
+      NODE_ENV: "dev",
+    },
+    //exec_mode: "cluster",
+    //instances: "max",
+    watch: false
+  },
+  {
+    name: "binance_database",
+    script: "./build/binance/database.js",
+    env_prod: {
+      NODE_ENV: "prod",
+    },
+    env_dev: {
+      NODE_ENV: "dev",
+    },
+    //exec_mode: "cluster",
+    //instances: "max",
+    watch: false
+  },
+  {
+    name: "server",
+    script: "./build/server.js",
+    env_prod: {
+      NODE_ENV: "prod",
+      PORT: 80
+    },
+    env_dev: {
+      NODE_ENV: "dev"
+    },
+    watch: false,
+    exec_mode: "cluster",
+    instances: "max",
+  },
+  {
+    name: "backtester",
+    script: "./build/backtester.js",
+    env_prod: {
+      NODE_ENV: "prod",
+    },
+    env_dev: {
+      NODE_ENV: "dev",
+    },
+    watch: false,
+  }],
 }
