846a0bc2ad9cf6dc1852216fcd0372030c6cee37
Sebastian Boehler <basti.boehler@hotmail.de>
2024-02-04 10:38:27 +0800

    fix: remove old exchanges

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,36 +1,28 @@
 import extendedEnv from 'dotenv-extended'
 import dotenvParseVariables from 'dotenv-parse-variables'
+import { logger } from '../utils'
 
+const envName = process.env.NODE_ENV?.split(' ').join('')
 const options = {
   silent: false,
   defaults: './src/config/.env.defaults',
   schema: './src/config/.env.schema',
-  path: `./src/config/.env.${process.env.NODE_ENV?.split(' ').join('')}`,
+  path: `./src/config/.env.${envName}`,
   includeProcessEnv: true,
   assignToProcessEnv: true,
   overrideProcessEnv: false,
 }
 
+logger.debug('loading environment', envName)
 const env = extendedEnv.load(options)
 const parsedConfig = dotenvParseVariables(env)
 
 const config = {
-  //BINANCE
-  BINANCE_API_KEY: parsedConfig.BINANCE_API_KEY as string,
-  BINANCE_API_SECRET: parsedConfig.BINANCE_API_SECRET as string,
-  BN_ENABLED_PAIRS: parsedConfig.BN_ENABLED_PAIRS as string[],
-  //COINBASE
-  CB_API_KEY: parsedConfig.CB_API_KEY as string,
-  CB_ENABLED_PAIRS: parsedConfig.CB_ENABLED_PAIRS as string[],
   //DYDX
   DYDX_ENABLED_PAIRS: parsedConfig.DYDX_ENABLED_PAIRS as string[],
   //DATABASE
   WRITE_TO_DB: parsedConfig.WRITE_TO_DB as boolean,
   MONGO_URL: parsedConfig.MONGO_URL as string,
-  //TRADING
-  LEVERAGE: parsedConfig.LEVERAGE as number,
-  //Back testing
-  START_INDEX: parsedConfig.START_INDEX as number,
   //NODE defaults
   NODE_ENV: parsedConfig.NODE_ENV as 'prod' | 'dev' | undefined,
   LOG_LEVEL: parsedConfig.LOG_LEVEL as string,
@@ -42,9 +34,6 @@ const config = {
   OKX_KEY: parsedConfig.OKX_KEY as string,
   OKX_SECRET: parsedConfig.OKX_SECRET as string,
   OKX_PASS: parsedConfig.OKX_PASS as string,
-
-  EXCHANGE: '',
-  SQL_PORT: null,
 }
 
 export default config
