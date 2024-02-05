924ef21838635d56337cd861f76e43789d2920fe
Sebastian Boehler <basti.boehler@hotmail.de>
2024-02-05 23:33:30 +0800

    fix: start capital

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,59 +1,35 @@
 module.exports = {
-  apps: [{
-    name: "dydx_database",
-    script: "./build/dydx/database.js",
-    env_prod: {
-      NODE_ENV: "prod"
+  apps: [
+    // {
+    //   name: "okx_database",
+    //   script: "./build/okx/database.js",
+    //   env_prod: {
+    //     NODE_ENV: "prod"
+    //   },
+    //   env_dev: {
+    //     NODE_ENV: "dev"
+    //   },
+    //   watch: true
+    // },
+    {
+      name: 'okx_trader_sol',
+      script: './build/okx/trader.js',
+      env_prod: {
+        NODE_ENV: "prod",
+        SYMBOL: 'SOL-USDT-SWAP',
+        START_CAPITAL: 400,
+      },
+      watch: false
     },
-    env_dev: {
-      NODE_ENV: "dev"
-    },
-    watch: true
-  },
-  {
-    name: "okx_database",
-    script: "./build/okx/database.js",
-    env_prod: {
-      NODE_ENV: "prod"
-    },
-    env_dev: {
-      NODE_ENV: "dev"
-    },
-    watch: true
-  },
-  {
-    name: "server",
-    script: "./build/server.js",
-    env_prod: {
-      NODE_ENV: "prod",
-      PORT: 80
-    },
-    env_dev: {
-      NODE_ENV: "dev"
-    },
-    watch: true,
-    exec_mode: "cluster",
-    instances: "max",
-  },
-  {
-    name: 'okx_trader_sol',
-    script: './build/okx/trader.js',
-    env_prod: {
-      NODE_ENV: "prod",
-      SYMBOL: 'SOL-USDT-SWAP',
-      START_CAPITAL: 400,
-    },
-    watch: false
-  },
-  {
-    name: 'okx_trader_sol',
-    script: './build/okx/trader.js',
-    env_prod: {
-      NODE_ENV: "prod",
-      SYMBOL: 'TIA-USDT-SWAP',
-      START_CAPITAL: 50,
-    },
-    watch: false
-  }
+    {
+      name: 'okx_trader_tia',
+      script: './build/okx/trader.js',
+      env_prod: {
+        NODE_ENV: "prod",
+        SYMBOL: 'TIA-USDT-SWAP',
+        START_CAPITAL: 80,
+      },
+      watch: false
+    }
   ],
 }
