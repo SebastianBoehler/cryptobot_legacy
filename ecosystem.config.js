daf304dabc0c6fcfaf2a15232694e1342fddf612
Sebastian Boehler <basti.boehler@hotmail.de>
2024-02-29 22:42:06 +0100

    chore: new trader on AR

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -75,6 +75,19 @@ module.exports = {
       },
       watch: false
     },
+    {
+      name: 'okx_trader_ar',
+      script: './build/okx/trader.js',
+      out_file: "/dev/null", //disable logs from being written to file
+      env_prod: {
+        NODE_ENV: "prod",
+        SYMBOL: 'AR-USDT-SWAP',
+        STRATEGY: 'BUILD_SCALP_FAST',
+        START_CAPITAL: 80,
+        MULTIPLIER: 0.95
+      },
+      watch: false
+    },
     //HB CAPITAL
     {
       name: 'hb_trader_ordi',
