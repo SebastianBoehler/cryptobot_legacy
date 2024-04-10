a6d9b1679c95495d1f45a742199376f0f34cc554
Sebastian Boehler <basti.boehler@hotmail.de>
2024-04-10 18:41:59 +0200

    chore: remove strat

--- src/strategies/index.ts
+++ src/strategies/index.ts
@@ -1,15 +1,13 @@
 import { BUILD_FAST } from './build_fast'
 import { BUILD_SCALP_FAST } from './build_scalp_fast'
-import { SCALP_FAST_NEW } from './scalp_fast_new'
 import { BUILD_SCALP_FAST_ALTS } from './scalp_fast_alts'
 import { SCALP_FAST_TEST } from './scalp_fast_test'
 import { BUILD_SCALP_FAST_INDICATORS } from './scalp_indicators'
 
 const build_fast = new BUILD_FAST()
 const build_scalp_fast = new BUILD_SCALP_FAST()
-const scalp_fast_new = new SCALP_FAST_NEW()
 const indicators = new BUILD_SCALP_FAST_INDICATORS()
 const alts = new BUILD_SCALP_FAST_ALTS()
 const test = new SCALP_FAST_TEST()
 
-export default { build_fast, build_scalp_fast, indicators, alts, test, scalp_fast_new }
+export default { build_fast, build_scalp_fast, indicators, alts, test }
