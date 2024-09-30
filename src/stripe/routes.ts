ab3dab7a64a186e69dda295c5c2c0fad7f30e269
Sebastian Boehler <basti.boehler@hotmail.de>
2024-09-30 09:58:41 +0200

    chore: push webhook

--- src/stripe/routes.ts
+++ src/stripe/routes.ts
@@ -1,28 +1,32 @@
 import express, { Router } from 'express'
 import Stripe from 'stripe'
-import config from '../config/config'
+//import config from '../config/config'
 import MongoWrapper from '../mongodb'
+import { logger } from '../utils'
 
 const mongo = new MongoWrapper('users')
 // await mongo.updateUserProfile('email', {})
 
 const router = Router()
-const stripe = new Stripe(config.STRIPE_KEY)
+//const stripe = new Stripe(config.STRIPE_KEY)
 
 router.post('/webhook', express.raw({ type: 'application/json' }), async (req, res) => {
   let event = req.body
   // Only verify the event if you have an endpoint secret defined.
   // Otherwise use the basic event deserialized with JSON.parse
   const signature = req.headers['stripe-signature'] || ''
-  const endpointSecret = 'whsec_JMjXwmg3eGbJ6kOTGqKAt18Vf6YXsehh'
+  //const endpointSecret = 'whsec_JMjXwmg3eGbJ6kOTGqKAt18Vf6YXsehh'
 
   try {
     console.log(typeof event, event, signature)
-    event = stripe.webhooks.constructEvent(req.body.toString(), signature, endpointSecret)
+    console.log(event.data.object)
+    //event = stripe.webhooks.constructEvent(req.body.toString(), signature, endpointSecret)
   } catch (err: any) {
     console.log(`⚠️  Webhook signature verification failed.`, err.message)
     return res.sendStatus(400)
   }
+
+  logger.debug('[stripe] webhook received', event)
 
   // Handle the event
   switch (event.type) {
